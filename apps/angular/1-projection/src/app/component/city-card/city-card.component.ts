f364d9c7459c00857d794aa11ef5c00def2b08cf
Weddly <weddly@hotmail.com>
2025-07-14 22:23:48 -0300

    feat(test): basic functionality

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,9 +1,32 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import { FakeHttpService } from '../../data-access/fake-http.service';
+import { CardType } from '../../model/card.model';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
-  imports: [],
+  template: `
+    <app-card [list]="cities()" [type]="cardType" customClass="bg-light-blue" />
+  `,
+  styles: [
+    `
+      ::ng-deep .bg-light-blue {
+        background-color: rgba(0, 0, 250, 0.1);
+      }
+    `,
+  ],
+  imports: [CardComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CityCardComponent {}
+export class CityCardComponent {
+  private http = inject(FakeHttpService);
+  private store = inject(CityStore);
+
+  cities = this.store.cities;
+  cardType = CardType.CITY;
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((s) => this.store.addAll(s));
+  }
+}
